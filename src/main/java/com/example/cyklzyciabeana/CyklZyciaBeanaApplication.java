a50ed0d5f666cfb40722a49dddedfc37a2facdc8
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2022-08-20 07:49:27 +0200

    bean lifecycle

---
+++ src/main/java/com/example/cyklzyciabeana/CyklZyciaBeanaApplication.java
@@ -0,0 +1,17 @@
+package com.example.cyklzyciabeana;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CyklZyciaBeanaApplication {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.register(AppConfiguration.class);
+		context.refresh();
+
+		LibraryManager manager = context.getBean(LibraryManager.class);
+		System.out.println("Context and beans are set up and ready to work");
+		context.close();
+	}
+
+}
---
+++ src/test/java/com/example/cyklzyciabeana/CyklZyciaBeanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cyklzyciabeana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyklZyciaBeanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
